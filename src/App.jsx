438d029ef0bd26439ccf20e72fc49789fc09188e
diveshdalvi <dalvidivesh1@gmail.com>
2023-11-24 23:52:22 +0530

    done with project

--- src/App.jsx
+++ src/App.jsx
@@ -7,50 +7,16 @@ import SearchResult from './Components/SearchResult';
 
 
 const App = () => {
-  const [searchResults , setSearchResults] = useState('');
   const [searchQuery,setSearchQuery] = useState('');
   const getSearchValue = useCallback((searchValue) => {
     setSearchQuery(searchValue);
-
-    console.log(searchQuery)
   }, []);
-  
-  
-      useEffect(() => {
-        const getData = async(searchQuery) => {
-          const options = {
-            method: 'GET',
-            url: 'https://google-search74.p.rapidapi.com/',
-            params: {
-              query: searchQuery,
-              limit: '3',
-              related_keywords: 'true'
-            },
-            headers: {
-              'X-RapidAPI-Key': import.meta.env.VITE_API_KEY,
-              'X-RapidAPI-Host': 'google-search74.p.rapidapi.com'
-            }
-          };
-          
-          try {
-            const response = await axios.request(options);
-            setSearchResults(response.data);
-            console.log(response.data)
-          } catch (error) {
-            console.error(error);
-          }
-        }
-        if (searchQuery !== '') {
-          
-        }
-      },[searchQuery])
-      
   return (
     <div  className="flex flex-col min-h-screen">
       <Navbar/>
       <div className="flex-1">
       <HomeSearchBox searchTextValue={getSearchValue}/>
-      <SearchResult searchResults={searchResults}/>
+      <SearchResult searchQuery={searchQuery}/>
       </div>
       <Footer/>
     </div>
--- src/Components/SearchResult.jsx
+++ src/Components/SearchResult.jsx
@@ -1,12 +1,46 @@
 import React, { useEffect, useState } from 'react'
+import Skeleton, { SkeletonTheme } from 'react-loading-skeleton';
 
-const SearchResult = ({searchResults}) => {
+import axios from 'axios'
+const SearchResult = ({searchQuery}) => {
+const [searchResult,setSearchResult] = useState([]);
+  useEffect(() => {
+    const getData = async() => {
+      const options = {
+        method: 'GET',
+        url: 'https://google-search74.p.rapidapi.com/',
+        params: {
+          query: searchQuery,
+          limit: '20',
+          related_keywords: 'true'
+        },
+        headers: {
+          'X-RapidAPI-Key': import.meta.env.VITE_API_KEY,
+          'X-RapidAPI-Host': 'google-search74.p.rapidapi.com'
+        }
+      };
+      
+      try {
+        const response = await axios.request(options);
+        setSearchResult(response.data.results)
+        console.log(response.data)
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    if (searchQuery !== '') {
+      getData()
+    }
+  },[searchQuery])
   return (
-    <div className=' flex justify-center pt-4 '>
-      <div className=' border-[#003554] flex flex-col flex-wrap w-5/6 sm:w-3/4 rounded-lg p-4 bg-base-300 shadow-lg'>
-      <h2 className='text-2xl '><a href="">Title</a></h2>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores sed sapiente inventore iste quibusdam nisi.</p>
+    <div className=' flex justify-center pt-4 flex-col '>
+      {searchResult.map(result => (
+        <div className=' border-[#003554] flex flex-col  w-5/6 sm:w-3/4 rounded-lg p-4 bg-base-300 shadow-lg border-2 m-auto mb-4'>
+      <h2 className='text-2xl mb-3 underline hover:text-blue-500'><a href={result.url} target='_blank'>{result.title || <Skeleton width={200}/>}</a></h2>
+      <p>{result.description || <Skeleton  count={5}/>}</p>
       </div>
+      ))}
+      
     </div>
   )
 }
